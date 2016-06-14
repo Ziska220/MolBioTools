141591ea39361ac1fd4e534f0392b1aaf3312a92
karly sindy <karlysindy@gmail.com>
2016-06-14 09:43:01 -0400

    fixed bug. forms are valid now and submittin to db.

--- match_oligo/views.py
+++ match_oligo/views.py
@@ -7,8 +7,8 @@ def import_excel_view(request):
     if request.method == "POST":
         form1 = UploadFileForm(request.POST, request.FILES)
 	form2 = RefForm(request.POST)
-        if form1.is_valid() and form2.is_valid():
-	    reference = form.save(commit=False)
+        if form1.is_valid() or form2.is_valid():
+	    reference = form2.save(commit=False)
 	    reference.save()
             filehandle = request.FILES['file']
 	    return render(request, 'match_oligo/test1.html', {'form2': form2})
@@ -16,8 +16,8 @@ def import_excel_view(request):
 	    return render(request, 'match_oligo/test2.html' , {'form2': form2})
 		
     else:
-	form1 = UploadFileForm(prefix="form1")
-	form2 = RefForm(prefix="form2")
+	form1 = UploadFileForm()
+	form2 = RefForm()
     
     return render(request, 'match_oligo/excel_input.html', {'form1': form1, 'form2': form2})
 
