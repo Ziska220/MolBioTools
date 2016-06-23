d749a4d94d9f2cb349fa7999f18ff0c8071ab644
karly sindy <karlysindy@gmail.com>
2016-06-22 21:08:37 -0400

    FIxed Two Bug: 1- Added .upper to reference. 2- Multiple oligo alignments can be found now. Changed elif to != -1 Did not realize .find() gave index, thought it gave 0 when found.

--- PyCharm_Oligo/convrt_combn_files.py
+++ PyCharm_Oligo/convrt_combn_files.py
@@ -6,16 +6,17 @@ import os
 import fileinput
 
 #opens and read lines of reference file
-ref_file = open('/Users/karlysindy/PycharmProjects/oligo_find/reference_file.txt')
+ref_file = open('/Users/karlysindy/Documents/django_test/oligo_db/PyCharm_Oligo/reference_file.txt')
 ref_read = ref_file.readline()
 #turns reference file into bioseq Seq object with string (eg upper) and biological methods (eg compliment)
-ref_seq = Seq(ref_read)
+ref_caps = ref_read.upper()
+ref_seq = Seq(ref_caps)
 #creates a variable assigned to the reverse compliment of the reference file
 ref_rev_comp = Seq.reverse_complement(ref_seq)
 
 #opens excel sheet
-book1 = xlrd.open_workbook('/Users/karlysindy/PycharmProjects/oligo_find/AZ Oligos.xlsx')
-book2 = xlrd.open_workbook('/Users/karlysindy/PycharmProjects/oligo_find/AJ Oligo Sheet.xls')
+book1 = xlrd.open_workbook('/Users/karlysindy/Documents/django_test/oligo_db/PyCharm_Oligo/AJ Oligo Sheet.xls')
+book2 = xlrd.open_workbook('/Users/karlysindy/Documents/django_test/oligo_db/PyCharm_Oligo/AZ Oligos.xlsx')
 #print "The number of worksheets is", book2.nsheets
 #print "Worksheet name(s):", book2.sheet_names()
 
@@ -31,36 +32,44 @@ book2 = xlrd.open_workbook('/Users/karlysindy/PycharmProjects/oligo_find/AJ Olig
 #print "Cell D30 is", sheet.cell_value(rowx=1, colx=1)
 
 #sets variables to starting row and column. need to add user input code for column or can I have it find the oligos?
-oligo_row = 0
-oligo_col = 2
-name_col = 1
+
 
 #summary: add +1 to oligo_row until reach nrows (the total number of rows in the sheet)
 
-f =('/Users/karlysindy/PycharmProjects/oligo_find/AZ Oligos.xlsx', '/Users/karlysindy/PycharmProjects/oligo_find/AJ Oligo Sheet.xls')
+f =('/Users/karlysindy/Documents/django_test/oligo_db/PyCharm_Oligo/AJ Oligo Sheet.xls', '/Users/karlysindy/Documents/django_test/oligo_db/PyCharm_Oligo/AZ Oligos.xlsx')
 for xlsfile in f:
-    print "enter for"
+    print ("enter for")
     book = xlrd.open_workbook(xlsfile)
     sheet = book.sheet_by_index(0)
     nrows = sheet.nrows
-    print nrows
+    print (nrows)
+
+    oligo_row = 0
+    oligo_col = 2
+    name_col = 1
+
     for oligo in range(sheet.nrows):
         cell = sheet.cell_value(rowx=oligo_row, colx=oligo_col)
-        print cell
+        print (cell)
+        print (xlsfile)
         if oligo_row < nrows:
-            print "if1"
+            print ("if1")
+            print (oligo_row)
             oligo_caps = cell.upper()
             oligo_find = ref_seq.find(oligo_caps)
             oligo_rev_find = ref_rev_comp.find(oligo_caps)
+            print (oligo_find)
+            print (oligo_caps)
             if oligo_find == -1 and oligo_rev_find == -1:
-                print "if2"
+                print ("if2")
                 oligo_row += 1
-            elif oligo_find ==0 or oligo_rev_find == 0:
-                print 'elif1'
+            elif oligo_find != -1 or oligo_rev_find != -1:
+                print ('elif1')
                 name = sheet.cell_value(rowx=oligo_row, colx=name_col)
-                print name
-                print oligo_caps
-                print "GOT IT"
+                oligo_row += 1
+                print (name)
+                print (oligo_caps)
+                print ("GOT IT")
 
 
 
