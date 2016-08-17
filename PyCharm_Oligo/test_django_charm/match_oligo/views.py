5e905a5134c6a51c99270f5ae5629cc0b1d96fa1
karly sindy <karlysindy@gmail.com>
2016-08-17 16:20:19 -0400

    Changed wording and formating of match_oligo output

--- PyCharm_Oligo/test_django_charm/match_oligo/views.py
+++ PyCharm_Oligo/test_django_charm/match_oligo/views.py
@@ -8,8 +8,6 @@ import re
 from .forms import UploadFileForm, RefForm, ChrLocForm
 #Access forms from forms.py
 
-
-
 def import_excel_view(request):
 #define function called 'import_excel_view'
     new_line_char = "--"
@@ -30,7 +28,7 @@ def import_excel_view(request):
 
             check = (form2.is_valid()), (form3.is_valid())
             if form1.is_valid() and all(check):
-                raise forms.ValidationError('Use only one reference')
+                raise forms.ValidationError('OOPS! You submitted two types of reference data. Either paste your reference or identify a chromosome location.')
                 #Raises error if there is user input for both references
 
             #SUBMITTED DATA: OLIGO FILE
@@ -39,6 +37,7 @@ def import_excel_view(request):
                 #Accesses 'file' from match_oligo/forms.py and uses .getlist to access all items in the MultiValueDict
                 name_match_list = []
                 sheet_info_list = []
+                reference_info = []
                 #creates empty  list where  matches from all files will be stored
                 ValidForm1 = True
                 #Grants entry into oligo search loop if True
@@ -54,6 +53,8 @@ def import_excel_view(request):
                 #uses biopython to convert reference into Seq object
                 ref_rev_comp = Seq.reverse_complement(ref_seq)
                 #uses biopython to create a reverse compliment of the submitted reference data
+                ref_length = str(len(ref_seq))
+                reference_info.extend(("Contains the following number of nucleotides: {}".format(ref_length),))
                 chr_input_seq = ''
                 chr_input_rev_seq = ''
                 #create empty list of UCSC das url reference variables to prevent error in oligo search loop
@@ -77,6 +78,7 @@ def import_excel_view(request):
                     #use biopython to create sequence object out of url text
                     chr_input_rev_seq = Seq.reverse_complement(chr_input_seq)
                     #use biopython to create reverse compiment of sequence
+                    reference_info.extend(("Chromosome {}: {}-{}".format(chrom,loc_start,loc_stop),))
                     ref_seq = ''
                     ref_rev_comp = ''
                     #create empty list of paste reference variables to prevent error in oligo search loop
@@ -86,7 +88,7 @@ def import_excel_view(request):
             for xlsfile in oligo_input:
             #iterates through user uploaded files
                 if new_line > 0:
-                    name_match_list.extend((new_line,))
+                    name_match_list.extend((new_line_char,))
                     #adds new line character if a file already had a match (new_line > 0)
                 new_line = 0
                 #reset- if a file does not have a match a new line character will not be added for next file
@@ -125,16 +127,15 @@ def import_excel_view(request):
                         oligo_find_url = chr_input_seq.find(oligo_caps)
                         oligo_rev_find_url = chr_input_rev_seq.find(oligo_caps)
                         #uses biopython to look for oligo in reference and reverse compliment of reference
-                        if oligo_find and oligo_find_url == -1 and oligo_rev_find and oligo_rev_find == -1 or cell == '':
+                        if oligo_find == -1 and oligo_find_url == -1 and oligo_rev_find == -1 and oligo_rev_find == -1 or cell == '':
                             oligo_row += 1
                             #if there is no match (-1), go to next row (add +1 to oligo_row)
-                        elif oligo_find or oligo_find_url != -1 or oligo_rev_find or oligo_rev_find_url != -1:
+                        elif oligo_find != -1 or oligo_find_url != -1 or oligo_rev_find != -1 or oligo_rev_find_url != -1:
                         #if there is a match (not -1, any other number is the index of the match), set handle to that cell name
                             name = sheet.cell_value(rowx=oligo_row, colx=name_col)
                             #assign handle to cell with match
                             name_match = str(name)
                             #create string from cell name
-
                             if saw_file < 1:
                                 xls_match_file_name = "%s:" % xlsfile
                                 name_match_list.extend((xls_match_file_name,))
@@ -147,7 +148,7 @@ def import_excel_view(request):
                             oligo_row += 1
                             new_line += 1
 
-            return render(request, 'match_oligo/output.html', {'var': name_match_list, 'search_param': sheet_info_list,})
+            return render(request, 'match_oligo/output.html', {'var': name_match_list, 'search_param': sheet_info_list, 'ref_info': reference_info})
 
     else:
         form1 = UploadFileForm()
